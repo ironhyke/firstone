30e42e5ce345e557db151cedbde7d9bb3a16c561
ironhyke <athulm717@gmail.com>
2025-11-02 12:55:36 +0530

    Update 5server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://ctrl-alt-elite-o5q5.vercel.app"
+  ],
+  credentials: true
+}));
+
 const mongoose = require("mongoose");
 require("dotenv").config();
 
@@ -13,10 +21,10 @@ const PORT = process.env.PORT || process.env.API_PORT;
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-  origin:  "ctrl-alt-elite-o5q5.vercel.app", 
-  credentials: true
-}));
+// app.use(cors({
+//   origin:  "ctrl-alt-elite-o5q5.vercel.app", 
+//   credentials: true
+// }));
 
 // register the routes
 app.use("/api/auth", authRoutes);
